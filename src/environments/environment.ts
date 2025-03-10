dc893ddfa989fdfc5f8aa168e616468c0a4912ff
Marcus Honório <108342348+Marcu0@users.noreply.github.com>
2025-03-10 11:42:54 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,14 +17,14 @@ export const environment = {
   production: false,
   firebaseConfig: {
     production: Boolean,
-    apiKey: "AIzaSyDoj4pBc1LDv01bLSqj-H85934cHHIk-2w",
-    authDomain: "camp-fe4fc.firebaseapp.com",
-    databaseURL: "https://camp-fe4fc-default-rtdb.firebaseio.com/",
-    projectId: "camp-fe4fc",
-    storageBucket: "camp-fe4fc.firebasestorage.app",
-    messagingSenderId: "236313934459",
-    appId: "1:236313934459:web:3a0c836973cadc08c280de",
-    measurementId: "G-ZZHQ8NFQGH"
+  apiKey: "XXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXXXXXXXX"
   } as FirebaseConfig
 };
 
